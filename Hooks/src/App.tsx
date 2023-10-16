281eeca38989d5eb008d60e26b63b14b6515c7c4
AKhil Kukadiya <73292704+Akhil1939@users.noreply.github.com>
2023-10-16 12:02:48 +0530

    use context implemented

--- Hooks/src/App.tsx
+++ Hooks/src/App.tsx
@@ -1,22 +1,22 @@
-
-import './App.css'
-import { UseState } from './components/useState'
-import { UseEffect } from './components/useEffect'
-import { CustomeHook } from './components/CustomeHook'
+import "./App.css";
+import { UseState } from "./components/useState";
+import { UseEffect } from "./components/useEffect";
+import { CustomeHook } from "./components/CustomeHook";
+import { UseContext } from "./components/Contexts/UseContext";
 
 function App() {
-
   return (
     <>
-    <p>basic Hooks</p>
-     <UseState/>
-     <hr/>
-     <UseEffect/>
-     <hr/>
-<CustomeHook/>
-        
+      <p>basic Hooks</p>
+      <UseState />
+      <hr />
+      <UseEffect />
+      <hr />
+      <CustomeHook />
+      <hr/>
+      <UseContext/>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
